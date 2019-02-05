d4a37b84765237e872fabfc067fb6b106ee58e16
Steve Albonico <salbonico@gmail.com>
2019-02-05 04:22:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,15 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    "My name is__"
+    "My name is "
   end
 
   get '/hometown' do
-    "My hometown is__"
+    "My hometown is "
   end
 
   get '/favorite-song' do
-    "My favorite sond is __"
+    "My favorite sond is "
   end
 
 
